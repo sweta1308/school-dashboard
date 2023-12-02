72237245999167031fbd071003c93a03bea20991
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-02 18:32:34 +0530

    feat: add school reducer

---
+++ src/reducers/SchoolReducer.js
@@ -0,0 +1,37 @@
+export const schoolReducer = (state, { type, payload }) => {
+  switch (type) {
+    case "SET_SCHOOLS":
+      return { ...state, schools: payload };
+    case "SEARCH_SCHOOL":
+      return { ...state, filters: { ...state.filters, searchTerm: payload } };
+    case "NAME_SORT":
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          nameSort: !state.filters.nameSort,
+          sortType: payload,
+        },
+      };
+    case "BOARD_SORT":
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          boardSort: !state.filters.boardSort,
+          sortType: payload,
+        },
+      };
+    case "MEDIUM_SORT":
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          mediumSort: !state.filters.mediumSort,
+          sortType: payload,
+        },
+      };
+    default:
+      return state;
+  }
+};
