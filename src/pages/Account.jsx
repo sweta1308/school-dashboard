e0571fe5977f14dc7ed976128a26fc41632aeb8f
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-02 18:31:53 +0530

    feat: add account page

---
+++ src/pages/Account.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { Sidebar } from "../components/Sidebar";
+import { useAuth } from "../context/AuthContext";
+import { useUser } from "../context/UserContext";
+import { UserModal } from "../components/UserModal";
+
+export const Account = () => {
+  const { loggedInUser } = useUser();
+  const { user } = useAuth();
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <>
+      {showModal && (
+        <UserModal
+          loggedInUser={loggedInUser}
+          user={user}
+          showModal={showModal}
+          setShowModal={setShowModal}
+        />
+      )}
+      <div style={{ filter: showModal ? "blur(8px)" : "" }}>
+        <Sidebar />
+        <div className="relative left-[20%] w-[80%] pt-[40px] rounded-xl lg:left-[30%] lg:w-[65%] md:left-0 md:w-full md">
+          <div className="flex flex-col gap-4 w-[300px] md:text-xs md:mx-auto">
+            <h1 className="font-bold text-[30px] md:text-center text-primary-color underline">
+              Profile
+            </h1>
+            {loggedInUser?.profilePicture ? (
+              <img
+                src={loggedInUser?.profilePicture}
+                alt="profile"
+                className="w-[150px] rounded-full md:w-[100px]"
+              />
+            ) : (
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWo3luud5KPZknLR5zdUUwzvYBztWgTxrkbA&usqp=CAU"
+                alt="profile"
+                className="w-[150px] rounded-full"
+              />
+            )}
+            <p>
+              <strong>Name: </strong>{" "}
+              {loggedInUser?.name ? loggedInUser?.name : "-"}
+            </p>
+            <p>
+              <strong>Email: </strong>
+              {loggedInUser?.email ? loggedInUser.email : user.email}
+            </p>
+            <p>
+              <strong>Username: </strong>{" "}
+              {loggedInUser?.username ? loggedInUser?.username : "-"}
+            </p>
+            <p>
+              <strong>Contact Number: </strong>{" "}
+              {loggedInUser?.contact ? loggedInUser?.contact : "-"}
+            </p>
+
+            <button
+              onClick={() => setShowModal(true)}
+              className="bg-primary-color text-white py-[10px] rounded hover:bg-primary-dark"
+            >
+              Edit
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
