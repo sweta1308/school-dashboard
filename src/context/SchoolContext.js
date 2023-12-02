d080e821c803382b715a2e38159e868df2a6f86c
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-02 18:30:32 +0530

    feat: add school context

---
+++ src/context/SchoolContext.js
@@ -0,0 +1,64 @@
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
+import { schoolReducer } from "../reducers/SchoolReducer";
+import { useAuth } from "./AuthContext";
+import { collection, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase/firebase";
+
+const SchoolContext = createContext();
+
+export const SchoolProvider = ({ children }) => {
+  const { user } = useAuth();
+  const initialState = {
+    schools: [],
+    filters: {
+      searchTerm: "",
+      sortType: "",
+      nameSort: false,
+      boardSort: false,
+      mediumSort: false,
+    },
+  };
+  const [initialSchoolData, setInitialSchoolData] = useState({
+    name: "",
+    medium: "",
+    board: "",
+    class: "",
+  });
+  const [schoolState, schoolDispatch] = useReducer(schoolReducer, initialState);
+
+  useEffect(() => {
+    if (user.uid) {
+      const unsubscribe = onSnapshot(collection(db, "school"), (snapshot) => {
+        schoolDispatch({
+          type: "SET_SCHOOLS",
+          payload: snapshot.docs.map((doc) => ({
+            ...doc.data(),
+            id: doc.id,
+          })),
+        });
+      });
+      return () => {
+        unsubscribe();
+      };
+    }
+  }, [user]);
+
+  const value = {
+    schoolState,
+    schoolDispatch,
+    initialSchoolData,
+    setInitialSchoolData,
+  };
+
+  return (
+    <SchoolContext.Provider value={value}>{children}</SchoolContext.Provider>
+  );
+};
+
+export const useSchool = () => useContext(SchoolContext);
