ab77b10283a677ff9143ccdadd90a20aad57b367
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-02 18:31:41 +0530

    feat: add providers

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
+import { SchoolProvider } from "./context/SchoolContext";
+import { UserProvider } from "./context/UserContext";
+import { SyllabusProvider } from "./context/SyllabusContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <UserProvider>
+          <SchoolProvider>
+            <SyllabusProvider>
+              <App />
+            </SyllabusProvider>
+          </SchoolProvider>
+        </UserProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
