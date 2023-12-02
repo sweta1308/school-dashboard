3912cc3c811a88669e14e47489ea4438ca2b8731
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-02 18:28:53 +0530

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import "./App.css";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
+import { RequireAuth } from "./components/RequireAuth";
+import { Dashboard } from "./pages/Dashboard";
+import { Syllabus } from "./pages/Syllabus";
+import { Account } from "./pages/Account";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        containerStyle={{
+          top: "1rem",
+          right: "1rem",
+          fontSize: "0.9rem",
+        }}
+      />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route element={<RequireAuth />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/syllabus" element={<Syllabus />} />
+          <Route path="/account" element={<Account />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
