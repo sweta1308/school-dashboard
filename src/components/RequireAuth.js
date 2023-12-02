83282d4d4bd87f8aa608530662020b6c64a09365
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-02 18:29:48 +0530

    feat: auth management

---
+++ src/components/RequireAuth.js
@@ -0,0 +1,13 @@
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+export function RequireAuth() {
+  const { isLoggedIn } = useAuth();
+  const location = useLocation();
+
+  return isLoggedIn ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
+}
