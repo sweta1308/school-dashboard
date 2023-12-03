bd2b91289033c58f1a4dc979cb1e8a5511e15a36
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-03 11:01:38 +0530

    style: change text color

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ export const Sidebar = () => {
   });
   return (
     <>
-      <div className="bg-white w-[15%] min-h-screen fixed flex flex-col gap-7 px-[20px] pt-[50px] text-primary-dark xl:text-[14px] lg:px-2 lg:text-[16px] md:flex-row md:justify-evenly md:w-full md:min-h-0 md:bottom-0 md:z-30 md:pt-2.5 md:py-1 md:pb-3.5 md:text-center md:text-sm md:shadow-md">
+      <div className="bg-white w-[15%] min-h-screen fixed flex flex-col gap-7 px-[20px] pt-[50px] xl:text-[14px] lg:px-2 lg:text-[16px] md:flex-row md:justify-evenly md:w-full md:min-h-0 md:bottom-0 md:z-30 md:pt-2.5 md:py-1 md:pb-3.5 md:text-center md:text-sm md:shadow-md">
         <div className="flex items-center md:hidden">
           {loggedInUser?.profilePicture ? (
             <img
@@ -27,34 +27,34 @@ export const Sidebar = () => {
               className="w-[50px] rounded-full md:w-[40px]"
             />
           )}
-          <span className="ml-[10px]">
+          <span className="ml-[10px] text-primary-dark">
             <strong>{loggedInUser?.name ? loggedInUser.name : "-----"}</strong>
           </span>
         </div>
         <NavLink
           style={getStyles}
           to="/"
-          className="py-[10px] pl-[8px] flex items-center gap-1 rounded-lg md:flex-col md:px-5 md:py-2 md:text-xs xs:px-2 hover:bg-primary-color hover:text-white"
+          className="text-primary-dark py-[10px] pl-[8px] flex items-center gap-1 rounded-lg md:flex-col md:px-5 md:py-2 md:text-xs xs:px-2 hover:bg-primary-color hover:text-white"
         >
           <GridView sx={{ fontSize: "20px" }} /> <span>Dashboard</span>
         </NavLink>
         <NavLink
           style={getStyles}
           to="/syllabus"
-          className="py-[10px] pl-[8px] flex items-center gap-1 rounded-lg md:flex-col md:px-5 md:py-2 md:text-xs xs:px-2 hover:bg-primary-color hover:text-white"
+          className="text-primary-dark py-[10px] pl-[8px] flex items-center gap-1 rounded-lg md:flex-col md:px-5 md:py-2 md:text-xs xs:px-2 hover:bg-primary-color hover:text-white"
         >
           <AddBox sx={{ fontSize: "20px" }} /> <span>Syllabus</span>
         </NavLink>
         <NavLink
           style={getStyles}
           to="/account"
-          className="py-[10px] pl-[8px] flex items-center gap-1 rounded-lg md:flex-col md:px-5 md:py-2 md:text-xs xs:px-2 hover:bg-primary-color hover:text-white"
+          className="text-primary-dark py-[10px] pl-[8px] flex items-center gap-1 rounded-lg md:flex-col md:px-5 md:py-2 md:text-xs xs:px-2 hover:bg-primary-color hover:text-white"
         >
           <AccountCircle sx={{ fontSize: "20px" }} /> <span>Account</span>
         </NavLink>
         <p
           onClick={() => logoutUser()}
-          className="cursor-pointer py-[10px] pl-[8px] flex items-center gap-1 rounded-lg md:flex-col md:px-5 md:py-2 md:text-xs xs:px-2 hover:bg-primary-color hover:text-white"
+          className="cursor-pointer text-red-600 py-[10px] pl-[8px] flex items-center gap-1 rounded-lg md:flex-col md:px-5 md:py-2 md:text-xs xs:px-2 hover:bg-red-500 hover:text-white"
         >
           <Logout sx={{ fontSize: "20px" }} /> <span>Log Out</span>
         </p>
