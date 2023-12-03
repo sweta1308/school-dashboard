705edf87166e1ac8ee658081ed7067c5a9f761ee
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-03 11:01:13 +0530

    feat: add restrict auth

---
+++ src/components/RestrictAuth.js
@@ -0,0 +1,13 @@
+import { Outlet, Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "../../context";
+
+export function RestrictAuth() {
+  const { isLoggedIn } = useAuth();
+  const location = useLocation();
+
+  return isLoggedIn ? (
+    <Navigate to={location?.state?.from?.pathname || "/"} replace />
+  ) : (
+    <Outlet />
+  );
+}
